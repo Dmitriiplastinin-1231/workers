5ebd5b90ba55f790a50031f11f08b97cc8f5131c
Dmitriiplastinin-1231 <dimanplastinen@gmail.com>
2023-05-17 19:21:52 +0500

    connect react and other to frontend develop

---
+++ client/src/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+import { Paths } from './paths';
+import Login from './pages/login/Login';
+import Register from './pages/register/register';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h1>Employees</h1>
+  },
+  {
+    path: Paths.login,
+    element: <Login />
+  },
+  {
+    path: Paths.register,
+    element: <Register />
+  },
+]);
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={ store }>
+      <RouterProvider router={ router } />
+    </Provider>
+  </React.StrictMode>
+);
+
+
+reportWebVitals();
---
+++ client/src/paths.ts
@@ -0,0 +1,9 @@
+export const Paths = {
+    home: '/',
+    employeeAdd: '/employees/add',
+    employeeEdit: '/employees/edit',
+    employee: '/emloyee',
+    status: '/status',
+    login: '/login',
+    register: '/register'
+} as const
